792142ecef6693af185ac026a16e43b8749f8f3a
Ivan Stepanovic <ivanstepanovic6@gmail.com>
2021-10-31 18:02:55 +0100

    commit

---
+++ handler/update.php
@@ -0,0 +1,25 @@
+<?php
+
+require "../dbBroker.php";
+require "../model/prijava.php";
+ 
+
+if(isset($_POST['katedra']) && isset($_POST['predmet']) 
+&& isset($_POST['sala']) && isset($_POST['datum'])){
+    
+    
+
+    $obj =new Prijava($_POST['id'],$_POST['predmet'],$_POST['katedra'],$_POST['sala'],$_POST['datum']);
+    $status=$obj->update($conn);
+
+
+    if($status){
+        echo 'Success';
+    }else{
+        echo $status;
+        echo "Failed";
+    }
+
+}
+
+?>
